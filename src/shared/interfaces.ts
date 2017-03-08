9b3ad95e29d1c825607baed54614fc34166d8faf
Peter Eskandar <peterwadid1@gmail.com>
2017-03-08 01:23:49 +0100

    'interfaces'

--- src/shared/interfaces.ts
+++ src/shared/interfaces.ts
@@ -1,40 +1,34 @@
 /**
  * Created by eskandar.peter on 06/03/2017.
  */
-export interface DenunciaAnteprima {
-  key: string;
-  title: string;
-  question: string;
-  category: string;
-  dateCreated: string;
-  user: IUser;
-  comments: number;
-}
-
 export interface DenunciaData {
-  key?: string;
-  thread: string;
-  text: string;
-  user: IUser;
+  placeName: string;
+  placeAddress: string;
+  location: Location;
   dateCreated: string;
-  votesUp: number;
-  votesDown: number;
+  tipologiaLocale:string;
+  descrizione:string;
+  userEmail: string;
 }
-
 
 export interface UserLocation{
-  latitude:string,
-  longitude:string,
-  country:string,
-  city:string,
-  address:string,
-  postalCode:string
+  latitude:string;
+  longitude:string;
+  country:string;
+  city:string;
+  address:string;
+  postalCode:string;
 }
 
 export interface IUser {
-  userID: string,
-  displayName: string,
-  userEmail: string,
-  picture: string,
-  accessToken: string
+  userID: string;
+  displayName: string;
+  userEmail: string;
+  picture: string;
+  accessToken: string;
+}
+
+export interface Location {
+  latitue:string;
+  longitude:string;
 }
